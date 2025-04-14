b936f4cd7f0a0f6b8e4ff85aebe846b4192d5130
Lucas Simonet <lucas.simonetdev@gmail.com>
2025-04-13 22:53:33 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,31 +1,41 @@
 import userData from '../fixtures/user-data.json'
+import DashboardPage from '../pages/dashboardPage';
+import LoginPage from '../pages/loginPage';
+import MenuPage from '../pages/menuPage';
+
+const loginPage = new LoginPage();
+const dashboardPage = new DashboardPage();
+const menuPage = new MenuPage();
 
 describe('Orange HRM Test', () => {
 
   const selectorsList = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton: '[type="submit"]',
-    sectionTitleTopBar:'.oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCrecentialAlert:'[role="alert"]',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-    firstNameField: '[name="firstName"]',
-    middleNameField: '[name="middleName"]',
-    lastNameField: '[name="lastName"]',
-    genericField: '.oxd-input--active',
-    dateField:'[placeholder="yyyy-dd-mm"]',
-    dateCloseButton: '.--close',
-    submitSaveButton: '[type="submit"]',
-
-
+   
+      sectionTitleTopBar:'.oxd-topbar-header-breadcrumb-module',
+      dashboardGrid: '.orangehrm-dashboard-grid',
+      firstNameField: '[name="firstName"]',
+      middleNameField: '[name="middleName"]',
+      lastNameField: '[name="lastName"]',
+      genericField: '.oxd-input--active',
+      dateField:'[placeholder="yyyy-dd-mm"]',
+      dateCloseButton: '.--close',
+      submitSaveButton: '[type="submit"]',
+      genericComboBox: '.oxd-select-text--active',
+      secondItemCombobox:'.oxd-select-dropdown > :nth-child(2)',
+      thirdItemCombobox:'.oxd-select-dropdown > :nth-child(3)'
   }
-
+  
   it.only('User Info Update - Sucess', () => {
-    cy.visit('/auth/login');
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username);
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password);
-    cy.get(selectorsList.loginButton).click();
+    loginPage.acessLoginPage();
+    loginPage.loginAnyWithUser(userData.userSucess.username, userData.userSucess.password);
+
+    dashboardPage.checkDashboardPage();
+
+    menuPage. acessMyInfo();
+
+
+
+    /*
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard');
     cy.get(selectorsList.myInfoButton).click();
@@ -35,13 +45,30 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.genericField).eq(3).clear().type('EmpIdTest');
     cy.get(selectorsList.genericField).eq(4).clear().type('OtrId');
     cy.get(selectorsList.genericField).eq(5).clear().type('DrivLicNum');
+
     cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10');
     cy.get(selectorsList.dateCloseButton).click();
+
+    cy.get(selectorsList.genericComboBox).eq(0).click();
+    cy.get(selectorsList.thirdItemCombobox).click();
+
+    cy.get(selectorsList.genericComboBox).eq(1).click();
+    cy.get(selectorsList.secondItemCombobox).click();
+
+    cy.get(selectorsList.dateField).eq(1).clear().type('2000-12-02');
+    cy.get(selectorsList.dateCloseButton).click();
+
     cy.get(selectorsList.submitSaveButton).eq(0).click();
     cy.get('body').should('contain','Successfully Updated');
     cy.get('.oxd-toast-close');
 
- 
+    cy.get(selectorsList.genericComboBox).eq(2).click();
+    cy.get(selectorsList.thirdItemCombobox).click();
+    cy.get('[options=""]').eq(0).click().clear().type('testee');
+
+    cy.get(selectorsList.submitSaveButton).eq(1).click();
+    cy.get('body').should('contain','Successfully Saved');
+    cy.get('.oxd-toast-close');
 
   });
   it ('Login - Fail', () => {
@@ -49,7 +76,7 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.usernameField).type(userData.userFail.username);
     cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
-    cy.get(selectorsList.wrongCrecentialAlert)
+    cy.get(selectorsList.wrongCrecentialAlert)*/
 
   })
 });
